a2d4aa639678f112ff47f7ab2a36014e22683986
NicPWNs <nic@nicpjones.com>
2022-04-10 20:14:49 -0400

    port-scan-detector added

--- port-scan-detector/port-scan-detector.py
+++ port-scan-detector/port-scan-detector.py
@@ -1,3 +1,30 @@
 #!/usr/bin/env python3
 # Author: Nic Jones (NicPWNs)
 # port-scan-detector.py
+
+import socket
+from datetime import datetime
+
+# Listen locally for an unsual port
+host = "0.0.0.0"
+port = 1023
+
+# Create socket
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind the socket
+server.bind((host,port))
+
+# Queue requests
+server.listen(5)
+print("Listening for TCP connections on: %s:%d..." % (str(host),port))
+
+while True:
+    client,addr = server.accept()
+
+    print("Hmm, that's an odd port to be connecting to. PORT SCANNING DETECTED ON PORT " + str(port))
+    print("Port scanning detected from: %s" % str(addr))
+
+    bye = "ACK!\r\n"
+    client.send(bye.encode('ascii'))
+    client.close()
