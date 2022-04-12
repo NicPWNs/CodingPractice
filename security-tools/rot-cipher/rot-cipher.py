e6d04e645a714c257a03a517ada6ba563f0afe16
NicPWNs <nic@nicpjones.com>
2022-04-12 17:59:59 -0400

    Rot cipher code!

---
+++ security-tools/rot-cipher/rot-cipher.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# Author: Nic Jones (NicPWNs)
+# rot-cipher.py
+
+textIn = str(input("\nEnter your text to rotate: "))
+rotNum = int(input("Enter rotation (ROT) cipher key [13]: ") or "13")
+textOut = ""
+
+for i in range(len(textIn)):
+    char = textIn[i]
+
+    if (char.isupper()):
+        textOut += chr((ord(char) + rotNum-65) % 26 + 65)
+
+    elif (char.islower()):
+        textOut += chr((ord(char) + rotNum-97) % 26 + 97)
+
+    else:
+        textOut += char
+
+print("\n.....Processing.....\n")
+print("[✅] Rotated text is: " + textOut)
+print("[🔑] Used key: " + str(rotNum))
+print("[❗] Note: Special characters are not rotated.\n")
